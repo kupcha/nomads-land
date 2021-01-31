336135aeb777e7e1a1c5d15768b6cb53ae8442ee
Patrick <patkupcha@gmail.com>
2021-01-30 22:25:10 -0500

    Committing my own files - beta access page

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("sup craig");
 });
 
-app.get('/', (req, res) => {
-    res.send("welcome craig");
-});
