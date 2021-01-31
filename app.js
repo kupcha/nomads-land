b2eebd56584f97698ecd1a9c461521c7420f3d5c
Patrick <patkupcha@gmail.com>
2021-01-30 23:55:23 -0500

    Updates to login js scripts

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-app.get('/accountLogin', (req, res) => {
-    res.render('accountLogin');
-})
+ 
+app.post('/accountLogin', (req, res) => { 
+    const{ psw } = req.body;
+    if (psw == 'bettercallmecraig'){
+        res.cookie('betaAccess', 'Y', { maxAge: 900000, httpOnly: true });
+        console.log('login cookie created');
+        res.render('accountLogin');
+    }else{
+        res.render('home');
+    }
+});
 
 
 const port = process.env.port || 3000;
