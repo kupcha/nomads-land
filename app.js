5bd95bd376e8946cb80b396bfd806a057f783708
Patrick <patkupcha@gmail.com>
2021-02-09 02:20:48 -0500

    login updates

--- app.js
+++ app.js
@@ -3,30 +3,33 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const dotenv = require('dotenv').config();
+
+const mongoose = require('mongoose');
+const User = require('./models/user.js');
+const { nextTick } = require('process');
+const mongoDB = "mongodb+srv://jimmy-nomad:bettercallmecraig@nomadsland.ss6yb.mongodb.net/nomadsland?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 const { auth } = require('express-openid-connect');
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
+  secret: 'KzUgcUH58ekEZE19szOQM-0oc8g5saJNGSYTSNCPTOXvsDUZgCE6iD507COrOHLS',
+  baseURL: 'https://nomadsland-env.eba-sgsdjyz3.us-east-1.elasticbeanstalk.com',
+  clientID: 'rilqQv6b8h1Tnr1NAXd8Kqe8uX48Bkay',
+  issuerBaseURL: 'https://dev-51-wtmvz.us.auth0.com'
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-
-
-const mongoose = require('mongoose');
-const User = require('./models/user.js');
-const { nextTick, env } = require('process');
-const mongoDB = "mongodb+srv://jimmy-nomad:bettercallmecraig@nomadsland.ss6yb.mongodb.net/nomadsland?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 
 app.set('view engine', 'ejs');
@@ -38,15 +41,51 @@ app.use(express.json());
 app.use(cookieParser());
 
 
+app.post('/', (req, res) => {
+    const { psw } = req.body;
+    if (psw == 'bettercallmecraig') {
+        res.cookie('access', 1, { maxAge: (3600000 * 24 * 30) });
+        res.redirect('login');
+    } else {
+        res.render('home');
+    }
+});
 
-// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
+    if (req.cookies.access){
+        res.redirect('login');
+    }else{
+        res.render('home');
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.render("login");
+})
 
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
+app.post('/signup', (req, res) => {
+    const{ username, psw, phone } = req.body;
+    res.send(username + " : " + psw )
+})
+
+app.get('/logo', (req, res) => {
+    res.send("<img src='images/jimmy-nomad-logo-square.jpeg'></img>");
+})
+
+app.get('/welcome', (req, res) => {
+    res.render('welcome');
+})
+
+app.get('/callback', (req, res) => {
+    res.redirect('welcome');
+})
 const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log(`listening on port ${port}...`);
+    console.log("app listening....");
 });
 
