75187b7b240a2e55cab3f73a793ca021a01d0215
Patrick <patkupcha@gmail.com>
2021-02-09 00:44:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const dotenv = require('dotenv').config();
-const { auth, requiresAuth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
 
-app.use(
-    auth({
-        issuerBaseURL: process.env.ISSUER_BASE_URL,
-        baseURL: process.env.BASE_URL,
-        clientID: process.env.CLIENT_ID,
-        secret: process.env.SECRET,
-        idpLogout: true,
-        authRequired: false
-    })
-);
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 const mongoose = require('mongoose');
 const User = require('./models/user.js');
