86d16e16789ab33fafc03b5c730fba5bfe20760c
Patrick <patkupcha@gmail.com>
2021-03-07 16:04:07 -0500

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+ 
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    elevation: {
+        type: Number,
+        required: true,
+        default: 0;
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
