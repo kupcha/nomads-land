ff2c5bbaca7ed10506839fd234bc17fa4b4c0111
Patrick <patkupcha@gmail.com>
2021-03-20 19:02:05 -0400

    Update homescreen.js

--- routes/homescreen.js
+++ routes/homescreen.js
@@ -133,29 +133,29 @@ router.post('/survey/recommendations', requiresAuth(), async function(req, res, 
 router.post('/thankyou', requiresAuth(), async function(req, res, next) {
   const userEmail = res.locals.user.email;
   const survey = req.body;
-
-  const activitySelection = survey.activitySelection;
-  const activityLocation = survey.activiytLocation;
-  let activityList = new Array(activitySelection.length);
-  for (var i = 0; i < activitySelection.length; i++){
-    const currRec = { type: activitySelection[i], location: activityLocation[i]};
-     activityList[i] = currRec;
-  }
-  const newSurvey = {
-    email : userEmail,
-    location : survey.location,
-    seasons : survey.seasons,
-    fun : survey.fun,
-    food : survey.food,
-    sights : survey.sights,
-    locals : survey.locals,
-    price : survey.price,
-    enviro : survey.enviro,
-    activityRecs : activityList,
-    mscEnviro : survey.mscEnvir
-  };
-  await db.collection('reviews').insertOne(newSurvey);
-  res.render('thankyou');
+res.send(survey);
+  // const activitySelection = survey.activitySelection;
+  // const activityLocation = survey.activiytLocation;
+  // let activityList = new Array(activitySelection.length);
+  // for (var i = 0; i < activitySelection.length; i++){
+  //   const currRec = { type: activitySelection[i], location: activityLocation[i]};
+  //    activityList[i] = currRec;
+  // }
+  // const newSurvey = {
+  //   email : userEmail,
+  //   location : survey.location,
+  //   seasons : survey.seasons,
+  //   fun : survey.fun,
+  //   food : survey.food,
+  //   sights : survey.sights,
+  //   locals : survey.locals,
+  //   price : survey.price,
+  //   enviro : survey.enviro,
+  //   activityRecs : activityList,
+  //   mscEnviro : survey.mscEnvir
+  // };
+  // await db.collection('reviews').insertOne(newSurvey);
+  // res.render('thankyou');
 
 
 
@@ -188,6 +188,6 @@ router.post('/thankyou', requiresAuth(), async function(req, res, next) {
   // //  activityList[i] = activitySelection[i] + ":" + activityLocation[i];
   // // }
   // res.send(formData);
-})
+});
 
 module.exports = router;
