2ad498ee7079fd776c32e1d5d8dd1b9164f97026
Patrick <patkupcha@gmail.com>
2021-03-26 16:25:18 -0400

    Update homescreen.js

--- routes/homescreen.js
+++ routes/homescreen.js
@@ -99,9 +99,30 @@ router.post('/survey', requiresAuth(), async function (req, res, next) {
   });
 });
 
-router.get('/test', requiresAuth(), function(req, res, next) {
-  res.render('test')
-})
+router.get('/test', requiresAuth(), async function (req, res, next) {
+  const userEmail = res.locals.user.email;
+  const currentNomad = await User.findOne({email: userEmail});
+  if (currentNomad){
+    res.render('test', {
+      username: currentNomad.username,
+      elevation: currentNomad.elevation,
+      trips: currentNomad.trips,
+      referrals: currentNomad.referrals,
+      userProfile: JSON.stringify(req.oidc.user, null, 2),
+      title: 'nomadsland'
+    })
+  }else{
+    const newUser = {
+      email : `${userEmail}`,
+      elevation : 0,
+      trips : 0,
+      referrals : 0,
+      shownAboutScreen : 0
+    }
+    db.collection('users').insertOne(newUser);
+    res.redirect('test');
+  }
+});
 
 
 router.post('/thankyou', requiresAuth(), async function(req, res, next) {
