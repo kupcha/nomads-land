a334fb9323bccdeb3af50148e0b565b08a383527
Patrick <patkupcha@gmail.com>
2021-03-29 01:15:36 -0400

    Update homescreen.js

--- routes/homescreen.js
+++ routes/homescreen.js
@@ -152,11 +152,11 @@ router.post('/survey/recommendations', requiresAuth(), async function(req, res, 
 });
 
 
-router.post('/thankyou', requiresAuth(), function(req, res, next) {
+router.post('/thankyou', requiresAuth(), async function(req, res, next) {
   const userEmail = res.locals.user.email;
   const survey = req.body;
   const recsMade = survey.recsMade;
-  
+
   const activitySelection = survey.activitySelection;
   const activityLocation = survey.activityLocation;
   let activityList = new Array(recsMade);
